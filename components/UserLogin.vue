ace343233d9a9c912fc6d42089aef289e99da476
Ignacio Mosca <ignaciomosca@gmail.com>
2024-08-22 22:40:12 -0300

    Add styles

--- components/UserLogin.vue
+++ components/UserLogin.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="container">
-    <h2 class="text-3xl font-bold underline">Login</h2>
-    <form @submit.prevent="login">
+  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Login</h2>
+    </div>
+    <form class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm space-y-6" @submit.prevent="login">
       <div class="form-group">
-        <label for="username">Username:</label>
-        <input type="text" id="username" v-model="username" class="form-control" />
+        <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username:</label>
+        <input type="text" id="username" v-model="username"  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
       </div>
       <div class="form-group">
-        <label for="password">Password:</label>
-        <input type="password" id="password" v-model="password" class="form-control" />
+        <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password:</label>
+        <input type="password" id="password" v-model="password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
       </div>
-      <button type="submit" class="btn btn-primary">Login</button>
+      <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Login</button>
     </form>
   </div>
 </template>
--- pages/operations/index.vue
+++ pages/operations/index.vue
@@ -1,26 +1,54 @@
 <template>
-  <div class="container">
-    <h2>Todo List</h2>
-    <form @submit.prevent="addTodo">
-      <div class="form-group">
-        <label for="title">Term 1:</label>
-        <input type="text" id="term1" v-model="term1" class="form-control" />
+  <div class="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div class="mx-auto max-w-2xl text-center">
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Perform Operation</h2>
+    </div>
+    <form @submit.prevent="addTodo" class="mx-auto mt-16 max-w-xl sm:mt-20">
+      <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+        <div class="sm:col-span-2">
+          <label for="term1" class="block text-sm font-semibold leading-6 text-gray-900">Term 1:</label>
+          <input
+            type="text"
+            id="term1"
+            v-model="term1"
+            class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+        <div class="sm:col-span-2">
+          <label for="operation" class="block text-sm font-medium text-gray-900">Choose an operation</label>
+          <select
+            id="operation"
+            v-model="operation"
+            name="operation"
+            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+          >
+            <option disabled value="">Select an operation</option>
+            <option v-for="op in operations" :key="op.id" :value="op.name">
+              {{ op.symbol }}
+            </option>
+          </select>
+        </div>
+        <div class="sm:col-span-2">
+          <label for="term2" class="block text-sm font-semibold leading-6 text-gray-900">Term 2:</label>
+          <input
+            type="text"
+            id="term2"
+            v-model="term2"
+            class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
       </div>
-      <div class="form-group">
-        <label for="operation">Operation:</label>
-        <input type="text" id="operation" v-model="operation" class="form-control" />
+      <div class="mt-10">
+        <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          Perform Operation
+        </button>
       </div>
-      <div class="form-group">
-        <label for="title">Term 2:</label>
-        <input type="text" id="term2" v-model="term2" class="form-control" />
-      </div>
-      <button type="submit" class="btn btn-primary">Perform Operation</button>
     </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue';
+import { defineComponent, ref } from 'vue';
 import axios from 'axios';
 
 interface Todo {
@@ -36,6 +64,14 @@ export default defineComponent({
     const term1 = ref<string>('');
     const term2 = ref<string>('');
     const operation = ref<string>('');
+    const operations = ref([
+      { name: "addition", symbol: "+", id: 1 },
+      { name: "subtraction", symbol: "-", id: 2 },
+      { name: "multiplication", symbol: "x", id: 3 },
+      { name: "division", symbol: "/", id: 4 },
+      { name: "squareRoot", symbol: "√", id: 5 },
+      { name: "randomString", symbol: "🎲", id: 6 },
+    ]);
 
     const addTodo = async () => {
       try {
@@ -48,6 +84,7 @@ export default defineComponent({
             Authorization: `Bearer ${localStorage.getItem('token')}`
           }
         });
+        // Clear the form fields after submission
         term1.value = '';
         term2.value = '';
         operation.value = '';
@@ -60,6 +97,7 @@ export default defineComponent({
       term1,
       term2,
       operation,
+      operations,
       addTodo
     };
   }
